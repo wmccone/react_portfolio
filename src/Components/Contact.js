2caa6aad36e0c3bff9d152ae12e4db56d9c62372
Walter M <waltermccone88@gmail.com>
2021-07-09 16:39:28 -0500

    remove superfluous files and add a favicon

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -32,7 +32,7 @@ class Contact extends Component {
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              <form action="mailto:gerrymccone88@gmail.com" method="post" id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
                     <label htmlFor="contactName">
